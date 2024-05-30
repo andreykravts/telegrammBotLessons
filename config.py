43d87600949a63fd6df5dadfcdadd479f25797a8
andreykravts <andreykravts2210@gmail.com>
2024-05-30 18:26:49 +0300

    telegrammBotLesson2 sqlMusic and bot py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 # guide on
 # https://www.siteguarding.com/en/how-to-get-telegram-bot-api-token
 token = '7112067289:AAEzbln8X3a9_fTUX-rM457kw_ksYCdSRzo'
+database_name = 'music.db'  # Файл с базой данных
+shelve_name = 'shelve.db'  # Файл с хранилищем
--- music.sql
+++ music.sql
@@ -6,9 +6,10 @@ CREATE TABLE IF NOT EXISTS "music" (
 	"wrong_answers"	TEXT,
 	PRIMARY KEY("Id")
 );
-INSERT INTO "music" ("Id","file_id","right_answer","wrong_answers") VALUES (1,'101','Imagine Dragons','Nogu Svelo'),
- (2,'102','Artic Monkeys','Lana Del Grey'),
- (3,'103','The Pixies','The Toxic Avenger'),
- (4,'104','The Black Eyed','Travis Scott'),
- (5,'105','Arabeqsque','Frank Dunwalle');
+INSERT INTO "music" ("Id","file_id","right_answer","wrong_answers")
+VALUES (1,'AwACAgQAAxkDAAM6ZliRTpmucrKiAAHgCwEGGq5WFTTiAAL7EgACOivIUiF2LmFuTLu5NQQ','Imagine Dragons','Nogu Svelo'),
+ (2,'AwACAgQAAxkDAAM4ZliRSw085I3X5IMTXmTCQeKuOAMAAvoSAAI6K8hSw-16GfdPyoc1BA','Artic Monkeys','Lana Del Grey'),
+ (3,'AwACAgQAAxkDAAM0ZliRRPJtlBD-9TKQDhLuufsGRnIAAvgSAAI6K8hSp4Sk1pHEbtM1BA','The Pixies','The Toxic Avenger'),
+ (4,'AwACAgQAAxkDAAMyZliRQRq3vnKIC00EE_s552DcLA8AAvcSAAI6K8hSqeF8xJqjkc41BA','The Black Eyed','Travis Scott'),
+ (5,'105','AwACAgQAAxkDAAM2ZliRR-LPl20c1S6qyc3ZHB1rO9sAAvkSAAI6K8hS0A07m0nacxQ1BA','Frank Dunwalle');
 COMMIT;
