c86e17b8a3f14fbe1a5a118734c683b2fbb1afc1
andreykravts <andreykravts2210@gmail.com>
2024-06-20 16:53:56 +0300

    telegramBotLesson5 vk auto posting in private channel with webhook

--- connection.sh
+++ connection.sh
@@ -1,1 +1,1 @@
-ssh "C:\awsKeys\awsdemo1.pem" ubuntu@ec2-52-204-201-31.compute-1.amazonaws.com -p 22
+ ssh -i "C:\awsKeys\awsdemo1.pem" ubuntu@ec2-34-224-222-93.compute-1.amazonaws.com -p 22
--- getPythonVenvPackeges.sh
+++ getPythonVenvPackeges.sh
@@ -1,4 +1,4 @@
-pip freeze > /home/ubuntu/tgbot/requirements.txt
+pip freeze > C:\Users\andre\PycharmProjects\telegrammBotLesson1\requerments.txt
 
 python3 -m venv /home/ubuntu/tgbot/venv
 source /home/ubuntu/tgbot/venv/bin/activate
