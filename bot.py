36bd3921f659dadf88a32a1a2cac1f48f8681a27
andreykravts <andreykravts2210@gmail.com>
2024-06-29 16:16:35 +0300

    telegramBotLesson5 vk auto posting in private channel with webhook

--- bot.py
+++ bot.py
@@ -112,40 +112,40 @@ if __name__ == '__main__':
 
 ###########################################################################
 
-class WebhookServer(object):
-    @cherrypy.expose
-    def index(self):
-        if 'content-length' in cherrypy.request.headers and \
-                        'content-type' in cherrypy.request.headers and \
-                        cherrypy.request.headers['content-type'] == 'application/json':
-            length = int(cherrypy.request.headers['content-length'])
-            json_string = cherrypy.request.body.read(length).decode("utf-8")
-            update = telebot.types.Update.de_json(json_string)
-            # Эта функция обеспечивает проверку входящего сообщения
-            bot.process_new_updates([update])
-            return ''
-        else:
-            raise cherrypy.HTTPError(403)
-
-# Снимаем вебхук перед повторной установкой (избавляет от некоторых проблем)
-bot.remove_webhook()
-
-# Ставим заново вебхук
-bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH,
-                certificate=open(config.WEBHOOK_SSL_CERT, 'r'))
-
-# Указываем настройки сервера CherryPy
-cherrypy.config.update({
-    'server.socket_host': config.WEBHOOK_LISTEN,
-    'server.socket_port': config.WEBHOOK_PORT,
-    'server.ssl_module': 'builtin',
-    'server.ssl_certificate': config.WEBHOOK_SSL_CERT,
-    'server.ssl_private_key': config.WEBHOOK_SSL_PRIV
-})
-
- # Собственно, запуск!
-cherrypy.quickstart(WebhookServer(), config.WEBHOOK_URL_PATH, {'/': {}})
-
-#Теперь запустим бесконечный цикл получения новых записей со стороны Telegram:
+# class WebhookServer(object):
+#     @cherrypy.expose
+#     def index(self):
+#         if 'content-length' in cherrypy.request.headers and \
+#                         'content-type' in cherrypy.request.headers and \
+#                         cherrypy.request.headers['content-type'] == 'application/json':
+#             length = int(cherrypy.request.headers['content-length'])
+#             json_string = cherrypy.request.body.read(length).decode("utf-8")
+#             update = telebot.types.Update.de_json(json_string)
+#             # Эта функция обеспечивает проверку входящего сообщения
+#             bot.process_new_updates([update])
+#             return ''
+#         else:
+#             raise cherrypy.HTTPError(403)
+#
+# # Снимаем вебхук перед повторной установкой (избавляет от некоторых проблем)
+# bot.remove_webhook()
+#
+# # Ставим заново вебхук
+# bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH,
+#                 certificate=open(config.WEBHOOK_SSL_CERT, 'r'))
+#
+# # Указываем настройки сервера CherryPy
+# cherrypy.config.update({
+#     'server.socket_host': config.WEBHOOK_LISTEN,
+#     'server.socket_port': config.WEBHOOK_PORT,
+#     'server.ssl_module': 'builtin',
+#     'server.ssl_certificate': config.WEBHOOK_SSL_CERT,
+#     'server.ssl_private_key': config.WEBHOOK_SSL_PRIV
+# })
+#
+#  # Собственно, запуск!
+# cherrypy.quickstart(WebhookServer(), config.WEBHOOK_URL_PATH, {'/': {}})
+#
+# #Теперь запустим бесконечный цикл получения новых записей со стороны Telegram:
 if __name__ == '__main__':
      bot.infinity_polling()
